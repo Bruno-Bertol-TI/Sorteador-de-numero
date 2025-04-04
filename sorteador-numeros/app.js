f61796e66e7c8bd67769c5693ad91badf299da3c
Bruno Bertol <bbertol2312@outlook.com>
2025-04-04 10:41:52 -0300

    capturando valores inseridos nos input

---
+++ sorteador-numeros/app.js
@@ -0,0 +1,7 @@
+function sortear() {
+    let qtdNumGerar = document.getElementById('quantidade').value;
+    let qtdNumGerarDe = document.getElementById('de').value;
+    let qtdNumGerarAte = document.getElementById('ate').value;
+
+    console.log(qtdNumGerar, qtdNumGerarDe, qtdNumGerarAte);
+}
